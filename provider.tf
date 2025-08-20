855a4e8d0b1693c66f5fc0ed962fe0a7b0dd968f
andrei-brando <albertbrando@hotmail.com>
2025-08-20 20:02:33 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "6.9.0"
     }
   }
+
+  backend "s3" {
+    bucket = "comunidadedevops-andrei"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
