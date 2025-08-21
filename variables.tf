ba4dcdb66366cd6abe9e9733381fa26f858306d1
andrei-brando <albertbrando@hotmail.com>
2025-08-21 07:41:44 -0300

    feat: add project_name var

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "cidr_block" {
   type        = string
   description = "Networking CIDR Block to be used for the VPC"
 }
+
+variable "project_name" {
+  type        = string
+  description = "Project Name to be used to name the resources (Name tag)"
+}
