5f1dd9f0d8d952873906f3656b6d64ea745b4aca
andrei-brando <albertbrando@hotmail.com>
2025-08-23 16:00:26 -0300

    refactor: terraform network module

---
+++ modules/network/variables.tf
@@ -0,0 +1,14 @@
+variable "cidr_block" {
+  type        = string
+  description = "Networking CIDR Block to be used for the VPC"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Project Name to be used to name the resources (Name tag)"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to be added to AWS resources"
+}
