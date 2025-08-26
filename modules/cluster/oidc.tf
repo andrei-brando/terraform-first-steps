2868a52f0c9f6569eac82f6346eca84a89847c09
andrei-brando <albertbrando@hotmail.com>
2025-08-26 08:41:28 -0300

    fix: resource name

--- modules/cluster/oidc.tf
+++ modules/cluster/oidc.tf
@@ -2,7 +2,7 @@ data "tls_certificate" "eks_oidc_tls_certificate" {
   url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
 }
 
-resource "aws_iam_openid_connect_provider" "example" {
+resource "aws_iam_openid_connect_provider" "eks_oidc" {
   client_id_list = [
     "sts.amazonaws.com"
   ]
