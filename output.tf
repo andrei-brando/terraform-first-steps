11a12533bd1d8bbce1393652ada8f033c415e52a
andrei-brando <albertbrando@hotmail.com>
2025-08-25 22:10:07 -0300

    feat: new outputs

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
-output "subnet_pub_1a" {
-  value = module.eks_network.subnet_pub_1a
-}
+# output "eks_vpc_config" {
+#   value = module.eks_cluster.eks_vpc_config
+# }
+
+# output "eks_oidc" {
+#   value = module.eks_cluster.eks_oidc
+# }
+
+# output "eks_oidc_tls_certificate" {
+#   value = module.eks_cluster.eks_oidc_tls_certificate
+# }
